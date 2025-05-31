6717e60d3f859a30dfbc753106c3b7efe52c8932
afril <fanka12afril@gmail.com>
2025-06-01 01:33:00 +0530

    frist commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function toddle(){
+  let sidebar = document.querySelector(".sidebar")
+  sidebar.style.display ="flex"
+}
+function closetoddle(){
+    let sidebar = document.querySelector(".sidebar")
+    sidebar.style.display ="none"
+}
